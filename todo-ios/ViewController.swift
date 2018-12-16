6e28339867bb930a186d2d994b4c96e68b623922
yuinchirn <l.serah.yuinch@gmail.com>
2018-12-16 17:30:36 +0900

    Initial Commit

---
+++ todo-ios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  todo-ios
+//
+//  Created by Yuta Chiba on 2018/12/16.
+//  Copyright © 2018 xxxx. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
